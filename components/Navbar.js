6cca9af29f98183fec45428700b3920793bbc145
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-09-09 17:01:30 +0530

    Created Home page and Navigation

---
+++ components/Navbar.js
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import styles from "@/app/page.module.css";
+import { usePathname } from "next/navigation";
+
+export default function Navbar() {
+    const pathname = usePathname();
+
+    return (
+        <nav className={styles.nav}>
+            <ul>
+                <li className={pathname === "/" ? styles.activepage : ""}>
+                    <Link href="/">Home</Link>
+                </li>
+                <li className={pathname === "/about" ? styles.activepage : ""}>
+                    <Link href="/about">About</Link>
+                </li>
+                <li className={pathname === "/blog" ? styles.activepage : ""}>
+                    <Link href="/blog">Blog</Link>
+                </li>
+                <li className={pathname === "/contact" ? styles.activepage : ""}>
+                    <Link href="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
